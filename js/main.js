c7c935ed60c2ab1923d3a398a3f24c294d6f6b29
Alexei1974 <68809162+Alexei1974@users.noreply.github.com>
2021-06-14 23:04:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,7 +1,36 @@
 $(function () {
-  var lazyLoadInstance = new LazyLoad({
 
+  var canUseWebP = function() {
+    var elem = document.createElement('canvas');
+
+    if (!!(elem.getContext && elem.getContext('2d'))) {
+        // was able or not to get WebP representation
+        return elem.toDataURL('image/webp').indexOf('data:image/webp') == 0;
+    }
+
+    // very old browser like IE 8, canvas not supported
+    return false;
+  };
+  var isWebpSupported = canUseWebP();
+
+  if (isWebpSupported === false) {
+    var lazyItems = document.querySelectorAll('[data-src-replace-webp]');
+
+    for (var i = 0; i < lazyItems.length; i += 1) {
+      var item = lazyItems[i];
+
+      var dataSrcReplaceWebp = item.getAttribute('data-src-replace-webp');
+      if (dataSrcReplaceWebp !== null) {
+        item.setAttribute('data-bg', dataSrcReplaceWebp);
+      }
+    }
+  }
+
+  var lazyLoadInstance = new LazyLoad({
+   
   });
+
+
   $('.customer__inner').slick({
     prevArrow: '<button class="customer__arrow-left" ><img src="images/arrow-slider-right.png" alt=""></button>',
     nextArrow: '<button class="customer__arrow-right" ><img src="images/arrow-slider-left.png" alt=""></button>',
